d3389936a74e845e27f1af982d8d503d37d6a02c
Cristian Salamea <ovnicraft@gmail.com>
2017-10-10 10:57:00 -0500

    [IMP] components

---
+++ src/components/Login.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="login" class="login mdl-layout__content">
+    <div class="mdl-grid">
+      <div class="mdl-cell mdl-cell--12-col">
+        <h4>Login</h4>
+      </div>
+      <div class="mdl-cell mdl-cell--12-col mdl-grid mdl-grid--no-spacing">
+        <form id="form-login" action="" method="post" >
+
+          <div class="mdl-cell mdl-cell--12-col">
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+              <input name="email" class="mdl-textfield__input" id="email" />
+              <label class="mdl-textfield__label" for="email">Email</label>
+            </div>
+            <div id="emailError" class="login-error"></div>
+          </div>
+
+          <div class="mdl-cell mdl-cell--12-col">
+            <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+              <input name="password" id="password" class="mdl-textfield__input" type="password"/>
+              <label class="mdl-textfield__label" for="password">Password</label>
+            </div>
+            <div id="passwordError" class="login-error"></div>
+          </div>
+
+          <div class="mdl-cell mdl-cell--12-col">
+            <button id="login" type="submit" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised mdl-button--colored">
+              Login
+            </button>
+            <button id="forgot" type="button" class="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--raised">
+              Forgot Password
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'Login',
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '@/pages/Home';
-import Login from '@/pages/Login';
+import Home from '@/components/Home';
+import Login from '@/components/Login';
+import About from '@/components/About';
 
 Vue.use(Router);
 
